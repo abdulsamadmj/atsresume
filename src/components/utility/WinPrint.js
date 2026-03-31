9658200380ea8f3aafaddbf693cd5ebfc9615c86
Abdul Samad M J <samadmj4@gmail.com>
2026-03-31 12:41:50 +0530

    chore: add GitHub link button

--- src/components/utility/WinPrint.js
+++ src/components/utility/WinPrint.js
@@ -1,3 +1,4 @@
+import { FaGithub } from "react-icons/fa";
 import { MdPictureAsPdf } from "react-icons/md";
 
 const WinPrint = () => {
@@ -7,13 +8,24 @@ const print = () => {
     };
 
 return (
-    <button
+    <>
+      <a
+        aria-label="Open GitHub"
+        className="exclude-print fixed bottom-20 right-10 font-bold rounded-full bg-white text-fuchsia-600 shadow-lg border-2 border-white"
+        href="https://github.com/abdulsamadmj/atsresume"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <FaGithub className="w-10 h-10" title="Open GitHub" />
+      </a>
+      <button
         aria-label="Download Resume"
         className="exclude-print fixed bottom-5 right-10 font-bold rounded-full bg-white text-fuchsia-600 shadow-lg border-2 border-white"
         onClick={print}
       >
-       <MdPictureAsPdf className="w-10 h-10" title="Download Resume"/>
+        <MdPictureAsPdf className="w-10 h-10" title="Download Resume" />
       </button>
+    </>
     );
 };
 
